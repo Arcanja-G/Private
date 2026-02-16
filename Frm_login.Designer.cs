f03f06c9b756c98bcbd77784ce295947b7018bdc
Arcanja <girlenearcanja.d@gmail.com>
2026-02-16 11:41:55 +0000

    Base de dados integrada

---
+++ Frm_login.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace ProjetoCSharp_BaseDados
+{
+    partial class Frm_login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_utilizador = new System.Windows.Forms.TextBox();
+            this.btn_login = new System.Windows.Forms.Button();
+            this.lbl_utilizador = new System.Windows.Forms.Label();
+            this.txt_senha = new System.Windows.Forms.TextBox();
+            this.lbl_senha = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.groupBox1.Controls.Add(this.panel2);
+            this.groupBox1.Controls.Add(this.panel1);
+            this.groupBox1.Controls.Add(this.flowLayoutPanel2);
+            this.groupBox1.Controls.Add(this.flowLayoutPanel1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txt_utilizador);
+            this.groupBox1.Controls.Add(this.btn_login);
+            this.groupBox1.Controls.Add(this.lbl_utilizador);
+            this.groupBox1.Controls.Add(this.txt_senha);
+            this.groupBox1.Location = new System.Drawing.Point(53, 39);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(239, 308);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel2.Location = new System.Drawing.Point(72, 165);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(143, 3);
+            this.panel2.TabIndex = 10;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Location = new System.Drawing.Point(72, 102);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(143, 3);
+            this.panel1.TabIndex = 9;
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(34, 143);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(32, 25);
+            this.flowLayoutPanel2.TabIndex = 8;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(34, 80);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(32, 25);
+            this.flowLayoutPanel1.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(78, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 32);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "LOGIN";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // txt_utilizador
+            // 
+            this.txt_utilizador.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.txt_utilizador.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_utilizador.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold);
+            this.txt_utilizador.ForeColor = System.Drawing.SystemColors.ScrollBar;
+            this.txt_utilizador.Location = new System.Drawing.Point(72, 80);
+            this.txt_utilizador.Name = "txt_utilizador";
+            this.txt_utilizador.ShortcutsEnabled = false;
+            this.txt_utilizador.Size = new System.Drawing.Size(102, 18);
+            this.txt_utilizador.TabIndex = 0;
+            this.txt_utilizador.Text = "Utilizador";
+            // 
+            // btn_login
+            // 
+            this.btn_login.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_login.Location = new System.Drawing.Point(60, 195);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(126, 43);
+            this.btn_login.TabIndex = 4;
+            this.btn_login.Text = "Entrar";
+            this.btn_login.UseVisualStyleBackColor = true;
+            this.btn_login.Click += new System.EventHandler(this.btn_login_Click);
+            // 
+            // lbl_utilizador
+            // 
+            this.lbl_utilizador.AutoSize = true;
+            this.lbl_utilizador.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.lbl_utilizador.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_utilizador.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lbl_utilizador.Location = new System.Drawing.Point(6, 56);
+            this.lbl_utilizador.Name = "lbl_utilizador";
+            this.lbl_utilizador.Size = new System.Drawing.Size(0, 21);
+            this.lbl_utilizador.TabIndex = 2;
+            // 
+            // txt_senha
+            // 
+            this.txt_senha.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.txt_senha.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_senha.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold);
+            this.txt_senha.ForeColor = System.Drawing.SystemColors.ScrollBar;
+            this.txt_senha.Location = new System.Drawing.Point(72, 143);
+            this.txt_senha.Name = "txt_senha";
+            this.txt_senha.Size = new System.Drawing.Size(102, 18);
+            this.txt_senha.TabIndex = 1;
+            this.txt_senha.Text = "Senha";
+            // 
+            // lbl_senha
+            // 
+            this.lbl_senha.AutoSize = true;
+            this.lbl_senha.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.lbl_senha.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_senha.Location = new System.Drawing.Point(3, 179);
+            this.lbl_senha.Name = "lbl_senha";
+            this.lbl_senha.Size = new System.Drawing.Size(0, 21);
+            this.lbl_senha.TabIndex = 8;
+            // 
+            // Frm_login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(361, 411);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lbl_senha);
+            this.Name = "Frm_login";
+            this.Text = "LOGIN";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        public System.Windows.Forms.TextBox txt_utilizador;
+        private System.Windows.Forms.Button btn_login;
+        private System.Windows.Forms.Label lbl_utilizador;
+        private System.Windows.Forms.TextBox txt_senha;
+        private System.Windows.Forms.Label lbl_senha;
+    }
+}
---
+++ Frm_login.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace ProjetoCSharp_BaseDados
+{
+    public partial class Frm_login : Form
+    {
+        MySqlConnection con;
+        MySqlCommand cmd;
+        MySqlDataReader dr;
+
+        public Frm_login()
+        {
+            InitializeComponent();
+            string minhaConString = ConfigurationManager.ConnectionStrings["minhaConnectionApp"].ToString();
+            con = new MySqlConnection(minhaConString);
+        }
+
+
+        private void btn_login_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+
+                string user = txt_utilizador.Text;
+                string pwd = txt_senha.Text;
+
+                string query = "Select * from login where lg_nome= @user and lg_pass = @pass";
+
+                using (MySqlConnection con = new MySqlConnection(ConfigurationManager.ConnectionStrings["minhaConnectionApp"].ConnectionString))
+                {
+                    using (MySqlCommand cmd = new MySqlCommand(query, con))
+                    {
+                        cmd.Parameters.AddWithValue("@user", user);
+                        cmd.Parameters.AddWithValue("@pass", pwd);
+
+                        con.Open();
+
+                        using (MySqlDataReader dr = cmd.ExecuteReader())
+                        {
+                            if (dr.Read())
+                            {
+                                MessageBox.Show("Login efetuado com sucesso!\n" + "Bom vindo " +dr[1],
+                                    "LOGIN", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                                //GerirForms.TrocarForms(this, new Frm_crud());
+                            }
+                            else
+                            {
+                                MessageBox.Show("Utilizador ou senha inválidos!", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            }
+                        }
+                    }
+                }
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocorreu um erro: " + ex.Message, "ERRO", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                //MessageBox.Show("Ocorreu um erro! Tente mais tarde!", "ERRO", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            /*catch(MySqlException ex) when (exNumber==0)  //nao foi possivel ligar ao servidor
+            {
+                MessageBox.Show("Não foi possivel ligar ao servidor");
+            }*/
+        }
+    }
+}
+
+
+            
+            
